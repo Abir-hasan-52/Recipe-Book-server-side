e1a6290f7f458d4d408445aebc2cfe3214e7a380
Abir-Hasan-52 <abirhasan5208+github@gmail.com>
2025-05-20 22:21:34 +0600

    recipe data deleted from server

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -41,6 +41,13 @@ async function run() {
       const result = await recipesCollection.insertOne(newRecipe);
       res.send(result);
     });
+
+    app.delete('/recipes/:id',async(req,res)=>{
+        const id =req.params.id;
+        const query={_id: new ObjectId(id)}
+        const result = await recipesCollection.deleteOne(query);
+        res.send(result);
+    })
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
