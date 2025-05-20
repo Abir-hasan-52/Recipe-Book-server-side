75a12916db1a000b8e38e1647722749fc8367ee6
Abir-Hasan-52 <abirhasan5208+github@gmail.com>
2025-05-20 23:26:35 +0600

    recipes celected id

--- index.js
+++ index.js
@@ -34,6 +34,13 @@ async function run() {
         const result = await cursor.toArray();
         res.send(result);
     })
+    app.get('/recipes/:id',async(req,res)=>{
+        const id = req.params.id;
+        const query={_id: new ObjectId(id)}
+        const result= await recipesCollection.findOne(query);
+        res.send(result);
+
+    })
 
     app.post("/recipes", async (req, res) => {
       const newRecipe = req.body;
