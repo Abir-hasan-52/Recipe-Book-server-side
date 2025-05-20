3dd540821f3359c638dd5759f7a51d43c291ff66
Abir-Hasan-52 <abirhasan5208+github@gmail.com>
2025-05-20 21:16:59 +0600

    recipes data get

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-const express = require('express');
-const cors = require('cors');
-require('dotenv').config();
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
+const { MongoClient, ServerApiVersion } = require("mongodb");
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -11,11 +11,7 @@ const port = process.env.PORT || 3000;
 app.use(cors());
 app.use(express.json());
 
-
-
- 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.kxazpdy.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
- 
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -23,7 +19,7 @@ const client = new MongoClient(uri, {
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
-  }
+  },
 });
 
 async function run() {
@@ -31,18 +27,26 @@ async function run() {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
 
-    const recipesCollection=client.db('recipeDB').collection('recipes');
+    const recipesCollection = client.db("recipeDB").collection("recipes");
 
-    app.post('/recipes',async(req,res)=>{
-        const newRecipe=req.body;
-        console.log(newRecipe);
-        const result = await recipesCollection.insertOne(newRecipe)
+    app.get('/recipes',async(req,res)=>{
+        const cursor=recipesCollection.find();
+        const result = await cursor.toArray();
         res.send(result);
     })
 
+    app.post("/recipes", async (req, res) => {
+      const newRecipe = req.body;
+      console.log(newRecipe);
+      const result = await recipesCollection.insertOne(newRecipe);
+      res.send(result);
+    });
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
@@ -50,13 +54,10 @@ async function run() {
 }
 run().catch(console.dir);
 
-
-
-app.get('/',(req,res)=>{
-    res.send('recipe server is cooking.')
+app.get("/", (req, res) => {
+  res.send("recipe server is cooking.");
 });
 
-app.listen(port,()=>{
-     console.log(`recipe server is running on port ${port}`)
-})
-
+app.listen(port, () => {
+  console.log(`recipe server is running on port ${port}`);
+});
